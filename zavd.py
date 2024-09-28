7583ec3717a037d0b4c7845e97d25f4a0199b323
belikmaks <142260216+belikmaks@users.noreply.github.com>
2024-09-28 17:48:40 +0300

    Add files via upload

---
+++ zavd.py
@@ -0,0 +1,9 @@
+institutions = {
+    "Школа": [1000, 1200, 350],
+    "Технікум": [200, 100],
+    "Училище": [200],
+}
+
+sum_school_students = sum(institutions["Школа"])
+
+print("Кількість учнів шкіл:", sum_school_students)
